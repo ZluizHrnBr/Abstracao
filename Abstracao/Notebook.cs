84fdde1c896a4b18a50b90ff631660a0459e7f85
Luiz Guilherme <Luiz Guilherme@DESKTOP-5NUNJ88>
2021-08-23 17:11:10 -0300

    Adicionar arquivos de projeto.

---
+++ Abstracao/Notebook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracao
+{
+    class Notebook
+    {
+        private string _nome;
+        public USB Porta1 { get; set; }
+        public USB Porta2 { get; set; }
+        public USB Porta3 { get; set; }
+        public string getNome()
+        {
+            return _nome;
+
+        }
+        public Notebook(string nome)
+        {
+            _nome = nome;
+
+        }
+
+    }
+
+}
+public abstract class USB
+{
+    public abstract void Plugar();
+}
+public class Iphone : USB
+{
+    public override void Plugar()
+    {
+        Console.WriteLine("Iphone plugado..");
+    }
+}
+public class Mouse : USB
+{
+    public override void Plugar()
+    {
+        Console.WriteLine("Mouse plugado..");
+    }
+}
+public class Teclado : USB
+{
+    public override void Plugar()
+    {
+        Console.WriteLine("Teclado plugado...");
+    }
+}
+
---
+++ Abstracao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Abstracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook sony = new Notebook("Sony Vaio");
+            sony.Porta1 = new Iphone();
+            var iphone = new Iphone();
+            iphone.Plugar();
+            sony.Porta2 = new Mouse();
+            var mouse = new Mouse();
+            mouse.Plugar();
+            sony.Porta3 = new Teclado();
+            var teclado = new Teclado();
+            teclado.Plugar();
+            Console.ReadLine();
+        }
+    }
+}
